0d754805184ae36a2a2896ba672dde79d5f10c67
Rogerio <rssampaio1176@gmail.com>
2020-03-09 20:51:57 -0300

    Projeto Pedido de Venda

---
+++ PedidoVenda/PedidoVenda/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace PedidoVenda.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ PedidoVenda/PedidoVenda/Program.cs
@@ -0,0 +1,27 @@
+﻿using PedidoVenda.Entities;
+using PedidoVenda.Entities.Enums;
+using System;
+
+namespace PedidoVenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1800,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txtStatus = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txtStatus);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}

fef51b0d6d49c1cf7a3f2acdd1e92748f0e78dd1
Rogerio <rssampaio1176@gmail.com>
2020-06-14 21:57:09 -0300

    Criação do modelo Department e controller, Initial do Entity e novo bootstrap-flatly

--- VendasWebMVC/VendasWebMVC/Startup.cs
+++ VendasWebMVC/VendasWebMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using VendasWebMVC.Data;
 
 namespace VendasWebMVC
 {
@@ -33,6 +35,10 @@ namespace VendasWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VendasWebMVCContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("VendasWebMVCContext"), 
+                        builder => builder.MigrationsAssembly("VendasWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
